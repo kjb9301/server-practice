4979e782775fa62fbb96e21b3f572f57b0be69ce
kjb9301 <kjb9301@naver.com>
2022-02-17 17:48:43 +0900

    feat: Setting db

---
+++ server/src/dbController.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+import { resolve } from "path";
+
+const basePath = resolve();
+
+const filenames = {
+  pets: resolve(basePath, "src/db/pets.json"),
+};
+
+export const readDB = (target) => {
+  try {
+    return JSON.parse(fs.readFileSync(filenames[target], "utf-8"));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const writeDB = (target, data) => {
+  try {
+    return fs.writeFileSync(filenames[target], JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
